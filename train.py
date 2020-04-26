3fd7d213b272e6ea2b65c1e860ba6e3d8c041148
Mahmood Khordoo <m.khordoo@gmail.com>
2020-04-25 18:19:11 -0600

    Implement Trainer class

--- train.py
+++ train.py
@@ -33,86 +33,128 @@ SAVE_CHECKPOINT_EVERY = 10
 LEARNING_RATE = 0.001
 EPOCHS = 100
 
-if __name__ == '__main__':
+os.makedirs(SAVE_DIR, exist_ok=True)
+tokenizer = Tokenizer(contractions_dict=contractions_dict)
 
-    os.makedirs(SAVE_DIR, exist_ok=True)
-    tokenizer = Tokenizer(contractions_dict=contractions_dict)
+
+def load_data():
+    """Loads the data and returns the cleaned,and tokenized data."""
     data_loader = dl.DialogLoaderTransformer(data_directory=dl.DATA_DIRECTORY,
                                              delimiter=dl.DELIMITER,
                                              movie_titles_headers=dl.MOVIES_TITLE_HEADERS,
                                              movie_lines_headers=dl.MOVIE_LINES_HEADERS,
                                              movie_conversation_headers=dl.MOVE_CONVERSATION_SEQUENCE_HEADERS)
-    # loading the data
+    # load and clean the  data
     source_texts, target_texts = data_loader.get_training_data(genre=GENRE, shuffle=True)
 
     tokenizer.fit_on_text(source_texts + target_texts, min_keep_frequency=MIN_TOKEN_FREQ)
+    # convert texts to numbers
     source_sequences = tokenizer.convert_text_to_number(source_texts)
     target_sequences = tokenizer.convert_text_to_number(target_texts)
     source_sequences, target_sequences = tokenizer.filter(source_numbers=source_sequences,
                                                           target_numbers=target_sequences,
                                                           max_token_size=MAX_TOKEN_LENGTH,
                                                           remove_unknown=True)
-
+    # convert numbers to tensors
     source_sequences = Utility.tensorize(source_sequences, dtype=torch.long, device=DEVICE)
     target_sequences = Utility.tensorize(target_sequences, dtype=torch.long, device=DEVICE)
 
+    return source_sequences, target_sequences
+
+
+class Trainer:
+    """A wrapper class for training"""
+
+    def __init__(self, util, device, save_checkpoint_every):
+        self.util = util
+        self.device = device
+        self.save_checkpoint_every = save_checkpoint_every
+        self.writer = SummaryWriter(comment='-' + datetime.now().isoformat(timespec='seconds'))
+
+    def train(self, encoder_decoder, tokenizer, train_source, train_target, test_source, test_target, learning_rate,
+              batch_size, teacher_forcing_ratio):
+        """Receives ab athc of soruce and target indexes and performances a decoding.For better performance we
+          encode all the sources at once in a batch. We will then use these encoded representations to
+          decode their corresponding targets sequentially.
+        """
+
+        optimizer = torch.optim.Adam(encoder_decoder.parameters(), lr=learning_rate)
+
+        for epoch in range(EPOCHS):
+            losses = []
+            blue_scores = []
+            for batch_source, batch_target in self.util.batch_generator(train_source, train_target,
+                                                                        batch_size=batch_size):
+                optimizer.zero_grad()
+                batch_encoder_outs, batch_encoder_hidden = encoder_decoder.encode(batch_source)
+                batch_decoder_outs = []
+                batch_target_indexes = []
+                bleu_sum = 0
+                # Sequentially decode every index in every target sequence
+                for position, target_sequence in enumerate(batch_target):
+                    encoder_hidden = self.util.get_batch_item(batch_encoder_hidden, position=position)
+                    decoder_hidden = encoder_hidden
+                    decoder_input = self.util.get_single_batch_tensor(tokenizer.sos_index, self.device)
+                    predicted_indexes = []
+                    for target_index in target_sequence:
+                        decoder_out, decoder_hidden = encoder_decoder.decode(decoder_input, decoder_hidden)
+                        predicted_index = decoder_out.argmax(dim=2)
+                        # teacher forcing
+                        if np.random.random() < teacher_forcing_ratio:
+                            decoder_input = self.util.get_single_batch_tensor(target_index, self.device)
+                        else:
+                            decoder_input = predicted_index
+
+                        batch_decoder_outs.append(decoder_out.squeeze(0))
+                        predicted_indexes.append(predicted_index.item())
+
+                    batch_target_indexes.extend(target_sequence)
+                    bleu_sum += self.util.belu_score(predicted_indexes,
+                                                     reference_sequences=target_sequence.cpu().numpy())
+
+                # Calculating the loss for every single index in the batch all at once
+                batch_decoder_outs = torch.cat(batch_decoder_outs).to(DEVICE)
+                batch_target_indexes = torch.LongTensor(batch_target_indexes).to(DEVICE)
+                loss = F.cross_entropy(batch_decoder_outs, batch_target_indexes)
+                loss.backward()
+                optimizer.step()
+                losses.append(loss.item())
+                bleu_sum /= len(batch_target)
+                blue_scores.append(bleu_sum)
+
+            mean_loss = np.array(losses).mean()
+            mean_bleu_train = np.array(blue_scores).mean()
+
+            mean_bleu_test = self.util.evaluate_net(encoder_decoder, test_source, test_target, tokenizer.sos_index,
+                                                    self.device)
+            self.report(epoch, mean_loss, mean_bleu_train, mean_bleu_test, encoder_decoder)
+        self.writer.close()
+
+    def report(self, epoch, mean_loss, mean_bleu_train, mean_bleu_test, encoder_decoder):
+        """Reports the metrics and also writes them to the tensorboard."""
+        logger.info(
+            f'Epoch:{epoch}, Mean loss:{mean_loss:.4f}, Mean BLEU:{mean_bleu_train:.4f}, Mean Test BLEU:{mean_bleu_test:.4f}')
+        self.writer.add_scalar('Mean_loss', mean_loss, epoch)
+        self.writer.add_scalar('Mean_Bleu', mean_bleu_train, epoch)
+        self.writer.add_scalar('Mean_Test_Bleu', mean_bleu_test, epoch)
+
+        if epoch % self.save_checkpoint_every == 0:
+            save_name = f"epoch_{epoch}_{datetime.now().isoformat(timespec='seconds')}.pt"
+            torch.save(encoder_decoder.state_dict(), os.path.join(SAVE_DIR, save_name))
+            logger.info(f'Checkpoint saved --> {save_name}')
+
+
+if __name__ == "__main__":
+    # Load the tokenized data
+    source_sequences, target_sequences = load_data()
+    # Create train and test sets
     train_source, train_target, test_source, test_target = Utility.split_train_test(source_sequences, target_sequences,
                                                                                     test_fraction=TEST_FRACTION)
-
     encoder_decoder = EncoderDecoder(vocab_size=tokenizer.dictionary_size,
                                      hidden_size=HIDDEN_SIZE,
                                      embedding_dim=EMBEDDING_DIM,
                                      bidirectional=False
                                      ).to(DEVICE)
-
-    optimizer = torch.optim.Adam(encoder_decoder.parameters(), lr=LEARNING_RATE)
-    writer = SummaryWriter(comment='-' + datetime.now().isoformat(timespec='seconds'))
-    for epoch in range(EPOCHS):
-        losses = []
-        blue_scores = []
-        for batch_source, batch_target in Utility.batch_generator(train_source, train_target, batch_size=BATCH_SIZE):
-            optimizer.zero_grad()
-            batch_encoder_outs, batch_encoder_hidden = encoder_decoder.encode(batch_source)
-            decoder_outs_batch = []
-            target_indexes_batch = []
-            bleu_sum = 0
-            for position, target_sequence in enumerate(batch_target):
-                encoder_hidden = Utility.get_batch_item(batch_encoder_hidden, position=position)
-                decoder_hidden = encoder_hidden
-                decoder_input = Utility.get_single_batch_tensor(tokenizer.sos_index, DEVICE)
-                predicted_indexes = []
-                for target_index in target_sequence:
-                    decoder_out, decoder_hidden = encoder_decoder.decode(decoder_input, decoder_hidden)
-                    predicted_index = decoder_out.argmax(dim=2)
-                    if np.random.random() < TEACHER_FORCING_PROB:
-                        decoder_input = Utility.get_single_batch_tensor(target_index, DEVICE)
-                    else:
-                        decoder_input = predicted_index
-
-                    decoder_outs_batch.append(decoder_out.squeeze(0))
-                    predicted_indexes.append(predicted_index.item())
-
-                target_indexes_batch.extend(target_sequence)
-                bleu_sum += Utility.belu_score(predicted_indexes, reference_sequences=target_sequence.cpu().numpy())
-
-            decoder_outs_batch = torch.cat(decoder_outs_batch).to(DEVICE)
-            target_indexes_batch = torch.LongTensor(target_indexes_batch).to(DEVICE)
-            loss = F.cross_entropy(decoder_outs_batch, target_indexes_batch)
-            loss.backward()
-            optimizer.step()
-            losses.append(loss.item())
-            bleu_sum /= len(batch_target)
-            blue_scores.append(bleu_sum)
-        mean_loss = np.array(losses).mean()
-        mean_bleu_train = np.array(blue_scores).mean()
-        mean_bleu_test = Utility.evaluate_net(encoder_decoder, test_source, test_target, tokenizer.sos_index, DEVICE)
-        logger.info(
-            f'Epoch:{epoch}, Mean loss:{mean_loss:.4f}, Mean BLEU:{mean_bleu_train:.4f}, Mean Test BLEU:{mean_bleu_test:.4f}')
-        writer.add_scalar('Mean_loss', mean_loss, epoch)
-        writer.add_scalar('Mean_Bleu', mean_bleu_train, epoch)
-        writer.add_scalar('Mean_Test_Bleu', mean_bleu_test, epoch)
-
-        if epoch % SAVE_CHECKPOINT_EVERY == 0:
-            save_name = f'epoch_{epoch}_blue_{mean_bleu_train:.3f}_bleu_test_{mean_bleu_test:.3f}.pt'
-            torch.save(encoder_decoder.state_dict(), os.path.join(SAVE_DIR, save_name))
-    writer.close()
+    trainer = Trainer(device=DEVICE, save_checkpoint_every=SAVE_CHECKPOINT_EVERY, util=Utility)
+    trainer.train(encoder_decoder, tokenizer, train_source, train_target, test_source, test_target,
+                  learning_rate=LEARNING_RATE, batch_size=BATCH_SIZE, teacher_forcing_ratio=TEACHER_FORCING_PROB)
